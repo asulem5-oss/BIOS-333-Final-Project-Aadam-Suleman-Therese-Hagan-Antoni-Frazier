549f1e12e01025d3a345ebf80f962afbb1cb0ebd
asulem5-oss <asulem5@uic.edu>
2026-05-06 15:47:16 -0500

    Updated Shiny app

--- ui.R
+++ ui.R
@@ -7,27 +7,38 @@
 #    https://shiny.posit.co/
 #
 
-library(shiny)
-
-# Define UI for application that draws a histogram
-fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
+ui <- fluidPage(
+  
+  titlePanel("BIOS 333 Final Project"),
+  
+  sidebarLayout(
+    
+    sidebarPanel(
+      h3("Controls"),
+      
+      selectInput(
+        inputId = "continent",
+        label = "Choose a Continent:",
+        choices = c("Africa", "Americas", "Asia", "Europe", "Oceania")
+      )
+    ),
+    
+    mainPanel(
+      h3("Plot Output"),
+      
+      plotOutput("lifePlot")
     )
+  )
 )
+
+server <- function(input, output) {
+  
+  output$lifePlot <- renderPlot({
+    hist(rnorm(100),
+         main = paste("Example Plot for", input$continent),
+         col = "skyblue",
+         border = "white")
+  })
+}
+
+shinyApp(ui = ui, server = server)
